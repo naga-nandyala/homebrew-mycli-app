bc0c1b4910b86f2b5f0e6aeb75fcaf5aa53f724b
Azure Pipeline <azure-pipeline@microsoft.com>
2026-01-28 05:13:30 +0000

    Update azure-cli-v3 to 2.77.0

--- Casks/azure-cli-v3.rb
+++ Casks/azure-cli-v3.rb
@@ -1,6 +1,6 @@
 cask "azure-cli-v3" do
   version "2.77.0"
-  sha256 "d16886cbe0681e0f1f6b9eb9133b1e0805bec96a7144d15d521880f7bae603fe"
+  sha256 "228115e9006a13f45cae735795b98caff43c859538b3035848b6ae6e05cc5587"
 
   url "https://github.com/naga-nandyala/azure-cli-pkg-1/releases/download/v2.77.0-v3/azure-cli-2.77.0-macos-arm64-nopython-signed-notarized.tar.gz"
   name "Azure CLI"
