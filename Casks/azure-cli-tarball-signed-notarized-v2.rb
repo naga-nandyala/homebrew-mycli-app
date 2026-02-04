255cbec46ccd866fab7ada0a9ff2f19b5992543e
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2026-02-04 11:49:55 +1100

    Update azure-cli-tarball-signed-notarized-v2.rb

--- Casks/azure-cli-tarball-signed-notarized-v2.rb
+++ Casks/azure-cli-tarball-signed-notarized-v2.rb
@@ -1,6 +1,6 @@
 cask "azure-cli-tarball-signed-notarized-v2" do
   version "2.77.0"
-  sha256 "5acf2277dbbdd046364e899d14f0ca4a9c2a25c5d76295d3dd472200e197dc61"
+  sha256 "6e41d9fa2dbe5d66d106719a9b244a18c9bbdd723ab52f65263da9601be3f3dd"
 
   url "https://github.com/naga-nandyala/azure-cli-pkg-1/releases/download/v2.77.0-tarball-v2/azure-cli-2.77.0-macos-arm64-signed-notarized.tar.gz"
   name "Azure CLI (Tarball - Signed & Notarized v2)"
