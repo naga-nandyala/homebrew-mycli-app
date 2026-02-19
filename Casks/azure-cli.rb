d51e1f33d898f577ee7a7432aa55b93cd0f9ab4f
Azure Pipeline <azure-pipeline@microsoft.com>
2026-02-19 05:00:21 +0000

    Update azure-cli to 2.77.0

--- Casks/azure-cli.rb
+++ Casks/azure-cli.rb
@@ -3,8 +3,8 @@ cask "azure-cli" do
   os macos: "macos"
 
   version "2.77.0"
-  sha256 arm:   "a83f8448d61cabd14c7e718ed4cb17e2b3a819076a03a08c3ff282dcd972ace8",
-         intel: "1f544b5d78b1bb65bc4b6b7fcba17e5a14134e05310ed61bae78d417374a7280"
+  sha256 arm:   "d1ac72660d271014e0ce0e3c4ffbd0a4b3e547834754f0245a6a3e6d904ff2e5",
+         intel: "262bd6c7f8f385dc2015809dafeee09c3ec3699c176e132aba44a8e044b20f33"
 
   url "https://github.com/naga-nandyala/azure-cli-pkg-1/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
   name "Azure CLI"
