3bc1db12a7e41ad15b84a083a30ad7ca914902ad
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2026-02-25 19:09:55 +1100

    2.83

--- Casks/azure-cli.rb
+++ Casks/azure-cli.rb
@@ -1,37 +1,12 @@
-# cask "azure-cli" do
-#   arch arm: "arm64", intel: "x86_64"
-#   os macos: "macos"
-
-#   version "2.83.0"
-#   sha256 arm:   "c852d72030273b76ef5e8f35cd665a4c3b578db1b2f73412faefee9cdbc18329",
-#          intel: "8cd6a47b698a77be4c8622916d47fa0660512fae0ce2748f8692a8b6be982fa6"
-
-#   url "https://github.com/naga-nandyala/azure-cli-latest/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
-#   name "Azure CLI"
-#   desc "Microsoft Azure CLI 2.0"
-#   homepage "https://docs.microsoft.com/cli/azure/overview"
-
-#   livecheck do
-#     url :url
-#     strategy :github_latest
-#   end
-
-#   depends_on formula: "python@3.13"
-
-#   binary "bin/az"
-
-#   zap trash: "~/.azure"
-# end
-################################################# DELETE below content after cask upgrade test ############
 cask "azure-cli" do
   arch arm: "arm64", intel: "x86_64"
   os macos: "macos"
 
-  version "2.77.0"
-  sha256 arm:   "b67c95d3fa1ec30917d9eedd853b6fc535a26302bec0b727dc39358f56902da5",
-         intel: "9826145058553f790dfe3f133eff0a63ba5aa17291545dbe50b357a7241f8cea"
+  version "2.83.0"
+  sha256 arm:   "c852d72030273b76ef5e8f35cd665a4c3b578db1b2f73412faefee9cdbc18329",
+         intel: "8cd6a47b698a77be4c8622916d47fa0660512fae0ce2748f8692a8b6be982fa6"
 
-  url "https://github.com/naga-nandyala/azure-cli-pkg-1/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
+  url "https://github.com/naga-nandyala/azure-cli-latest/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
   name "Azure CLI"
   desc "Microsoft Azure CLI 2.0"
   homepage "https://docs.microsoft.com/cli/azure/overview"
@@ -47,3 +22,28 @@ cask "azure-cli" do
 
   zap trash: "~/.azure"
 end
+################################################# DELETE below content after cask upgrade test ############
+# cask "azure-cli" do
+#   arch arm: "arm64", intel: "x86_64"
+#   os macos: "macos"
+
+#   version "2.77.0"
+#   sha256 arm:   "b67c95d3fa1ec30917d9eedd853b6fc535a26302bec0b727dc39358f56902da5",
+#          intel: "9826145058553f790dfe3f133eff0a63ba5aa17291545dbe50b357a7241f8cea"
+
+#   url "https://github.com/naga-nandyala/azure-cli-pkg-1/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
+#   name "Azure CLI"
+#   desc "Microsoft Azure CLI 2.0"
+#   homepage "https://docs.microsoft.com/cli/azure/overview"
+
+#   livecheck do
+#     url :url
+#     strategy :github_latest
+#   end
+
+#   depends_on formula: "python@3.13"
+
+#   binary "bin/az"
+
+#   zap trash: "~/.azure"
+# end
