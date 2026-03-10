35360aae3f2412d5c4a3bf5e94ce8f246038fcab
Azure Pipeline <azure-pipeline@microsoft.com>
2026-03-10 00:52:50 +0000

    Update azure-cli to 2.84.0

---
+++ Casks/a/azure-cli.rb
@@ -0,0 +1,24 @@
+cask "azure-cli" do
+  arch arm: "arm64", intel: "x86_64"
+  os macos: "macos"
+
+  version "2.84.0"
+  sha256 arm:   "1f173c4cae0321ce6546eef2aeabef099e016c1715b1473ea8536595b8a07ed6",
+         intel: "c793fb0a6e700ea6c5548e9f685ffbca8178f7f3aec5d8c94353f6c5e10abd23"
+
+  url "https://github.com/naga-nandyala/azure-cli-broker-new/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
+  name "Azure CLI"
+  desc "Microsoft Azure CLI 2.0"
+  homepage "https://docs.microsoft.com/cli/azure/overview"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on formula: "python@3.13"
+
+  binary "bin/az"
+
+  zap trash: "~/.azure"
+end
