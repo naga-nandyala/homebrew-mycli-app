a991d9071fab7d459cba76151a64bb25fdb954ac
Azure Pipeline <azure-pipeline@microsoft.com>
2026-03-11 18:36:13 +0000

    Update azure-cli to 2.84.0

--- Casks/a/azure-cli.rb
+++ Casks/a/azure-cli.rb
@@ -3,8 +3,8 @@ cask "azure-cli" do
   os macos: "macos"
 
   version "2.84.0"
-  sha256 arm:   "04c29951f02b4502734e9d08b0e7a6c6facee4ee278c42cf2da6e999bcd5c812",
-         intel: "051e04641df5329e847a0f45001c940d6d9f2a6da5ae7977c43c7eb92f2a9920"
+  sha256 arm:   "0aa6ad99e7cac0d2a6120d790fc71d7e0fe7735f33669deb1cda154dc404bc6f",
+         intel: "d7f1e62d543a2604bffbfe056ef9d8dc7121e6215d4bd606e1ff871c7f590061"
 
   url "https://github.com/naga-nandyala/azure-cli-broker-new/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
   name "Azure CLI"
@@ -25,27 +25,3 @@ cask "azure-cli" do
 
   zap trash: "~/.azure"
 end
-# cask "azure-cli" do
-#   arch arm: "arm64", intel: "x86_64"
-#   os macos: "macos"
-
-#   version "2.83.0"
-#   sha256 arm:   "c852d72030273b76ef5e8f35cd665a4c3b578db1b2f73412faefee9cdbc18329",
-#          intel: "8cd6a47b698a77be4c8622916d47fa0660512fae0ce2748f8692a8b6be982fa6"
-
-#   url "https://github.com/naga-nandyala/azure-cli-latest/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
-#   name "Azure CLI"
-#   desc "Microsoft Azure CLI 2.0"
-#   homepage "https://docs.microsoft.com/cli/azure/overview"
-
-#   livecheck do
-#     url :url
-#     strategy :github_latest
-#   end
-
-#   depends_on formula: "python@3.13"
-
-#   binary "bin/az"
-
-#   zap trash: "~/.azure"
-# end
