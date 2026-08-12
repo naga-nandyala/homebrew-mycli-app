7e4fcfd00444d54f3c8b6b7a9e66cb2d9d30c921
Azure Pipeline <azure-pipeline@microsoft.com>
2026-08-12 02:10:33 +0000

    Update azure-cli to 2.89.0

--- Casks/a/azure-cli.rb
+++ Casks/a/azure-cli.rb
@@ -1,12 +1,15 @@
 cask "azure-cli" do
   arch arm: "arm64", intel: "x86_64"
+  os macos: "macos", linux: "linux"
 
-  version "2.88.0"
-  sha256 arm:   "30f27075dc8bf965a15e05381fbbae42ca4060326100c6d097581cd0e330b251",
-         intel: "8945009a23dcd7b3a26c2dda26948c0fd70478343b4d8fe19e2e75e4135e8c82"
+  version "2.89.0"
+  sha256 arm:   "f8796ec9e61a989aad58c29de0f0f844c0f71f44abcf2284c1f1ae156b8d0e1b",
+    intel: "998e04b09a0cf0a7381cfc02e56b1d4dd33ba26f2fdba7364c3735d85539c496",
+    arm64_linux: "35a71b2212f3d4c504e30ce4b4437d5e573751319bd9fded88127e69b0c8ab80",
+    x86_64_linux: "0bd8b0e3cf3f46b88bb944dfa3497b898611162666793ab2a4dac38fb2e41f29"
 
-  url "https://github.com/naga-nandyala/azure-cli-broker-2/releases/download/azure-cli-#{version}/azure-cli-#{version}-macos-#{arch}.tar.gz",
-      verified: "github.com/naga-nandyala/azure-cli-broker-2/"
+  url "https://github.com/naga-nandyala/azure-cli/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz",
+      verified: "github.com/naga-nandyala/azure-cli/"
   name "Azure CLI"
   desc "Microsoft Azure CLI 2.0"
   homepage "https://docs.microsoft.com/cli/azure/overview"
